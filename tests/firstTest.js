443c6b2e85013293237ae6b66317f6c1ee4436fc
Mette Nygård <mette.nygard@tuni.fi>
2024-11-20 13:58:21 +0200

    first test

---
+++ tests/firstTest.js
@@ -0,0 +1,9 @@
+// sum.js 
+function sum(a, b) 
+{ return a + b; } 
+
+module.exports = sum;
+
+const sum = require('./sum'); 
+test('adds 1 + 2 to equal 3', () =>
+ { expect(sum(1, 2)).toBe(3); });
