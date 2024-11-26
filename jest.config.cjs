04641fbdd4d42c345ccbc5aee997674459515a4a
Mikko Haikonen <mikko.haikonen@tuni.fi>
2024-11-26 13:59:36 +0200

    Some more changes

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,5 +1,12 @@
-module.exports = 
-{ 
-    "collectCoverage": true, 
-    "coverageReporters": ["lcov", "text"] 
+module.exports = {
+  collectCoverage: true,
+  collectCoverageFrom: [
+    "src/**/*.{js,jsx,ts,tsx}",
+    "tests/**/*.{js,jsx,ts,tsx}" // Adjust if needed
+  ],
+  coverageReporters: ["lcov", "text"],
+  testMatch: [
+    "**/tests/**/*.test.js",    // This matches any .test.js files in the tests folder
+    "**/tests/**/*.spec.js"     // This matches any .spec.js files in the tests folder
+  ]
 };
