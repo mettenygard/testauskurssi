8695549dec04d89908c99ac0f8c362c63a1498e5
Mette Nygård <mette.nygard@tuni.fi>
2024-12-10 19:10:36 +0200

    divide ceil tests

--- tests/ceilTest.spec.js
+++ tests/ceilTest.spec.js
@@ -1,18 +1,33 @@
 import ceil from "../src/ceil";
 
 describe("ceil tests", () =>{
-    it("round to nearest integer", () =>{
+    it("round decimal to nearest integer", () =>{
         expect(ceil(10.045)).toBe(11);
-        expect(ceil(10.545, 0)).toBe(11);
-        expect(ceil(9,0)).toBe(9);
-        expect(ceil(8)).toBe(8);
-        expect(ceil(168,-1)).toBe(170);
-        expect(ceil(6040005,-2)).toBe(6100000);
+        expect(ceil(10.545, 0)).toBe(11);     
     });
 
-    it("round to nearest desimal", () => {
+    it("round integer to integer returns same value", () =>{
+        expect(ceil(9,0)).toBe(9);
+        expect(ceil(8)).toBe(8);
+    })
+
+    it("round 3 number integer to precice 2 integer", () =>{
+        expect(ceil(168,-1)).toBe(170);
+    })
+
+    it("round big integers to precice 2 integer", () =>{
+        expect(ceil(6040005,-2)).toBe(6100000);
+    })
+
+    it("round decimal to nearest desimal", () => {
         expect(ceil(10.045,2)).toBe(10.05);
+    })
+
+    it("round 9.99 decimal to 10.00 decimal", () =>{
         expect(ceil(9.998,2)).toBe(10.00);
+    })
+
+    it("round decimal to 9.99 decimal", () =>{
         expect(ceil(9.9877,2)).toBe(9.99);
     })
 });
