dde423e2436aa2b8716050be595b0be390da64b1
Mette Nygård <mette.nygard@tuni.fi>
2024-12-11 09:07:40 +0200

    add more compact tests

--- tests/compactTest.spec.js
+++ tests/compactTest.spec.js
@@ -5,7 +5,54 @@ describe("compact tests", () =>{
         const array = [1,2,3,4];
         //const array = [0, 1, false, 2, '', 3];
         const result = compact(array)
-        //expect(result.length).toBe(4);
-        expect(result).toEqual([2,3,4]);
+        expect(result).toEqual([1,2,3,4]);
+    });
+
+    it('remove 0', () => {
+        const array = [0,1,2,3,4];
+        const result = compact(array)
+        expect(result).toEqual([1,2,3,4]);
+    });
+
+    it('remove ""', () => {
+        const array = ["",1,2,3,4];
+        const result = compact(array)
+        expect(result).toEqual([1,2,3,4]);
+    });
+
+    it('remove null', () => {
+        const array = [null,1,2,3,4];
+        const result = compact(array)
+        expect(result).toEqual([1,2,3,4]);
+    });
+
+    it('remove undefined', () => {
+        const array = [undefined,1,2,3,4];
+        const result = compact(array)
+        expect(result).toEqual([1,2,3,4]);
+    });
+
+    it('remove NaN', () => {
+        const array = [NaN,1,2,3,4];
+        const result = compact(array)
+        expect(result).toEqual([1,2,3,4]);
+    });
+
+    it('remove false', () => {
+        const array = [false,1,2,3,4];
+        const result = compact(array)
+        expect(result).toEqual([1,2,3,4]);
+    });
+
+    it('not remove true', () => {
+        const array = [true,1,2,3,4];
+        const result = compact(array)
+        expect(result).toEqual([true,1,2,3,4]);
+    });
+
+    it('remove all and return empty list', () => {
+        const array = [NaN,0,null,undefined,"", false];
+        const result = compact(array)
+        expect(result).toEqual([1,2,3,4]);
     });
 });
