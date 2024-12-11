968296d49c44214b95408c66964c4f3bfbfe7971
Mette Nygård <mette.nygard@tuni.fi>
2024-12-11 12:33:29 +0200

    remove comment

--- tests/compactTest.spec.js
+++ tests/compactTest.spec.js
@@ -3,7 +3,6 @@ import compact from "../src/compact";
 describe("compact tests", () =>{
     it('return all', () => {
         const array = [1,2,3,4];
-        //const array = [0, 1, false, 2, '', 3];
         const result = compact(array)
         expect(result).toEqual([1,2,3,4]);
     });
